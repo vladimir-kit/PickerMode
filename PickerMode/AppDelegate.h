311046ea9b848a6660ebdfa7d3901c953fceaab9
Alex <alex.kovalskiy@icloud.com>
2014-03-04 23:32:28 +0200

    Initial Commit

---
+++ PickerMode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PickerMode
+//
+//  Created by Alex on 3/4/14.
+//  Copyright (c) 2014 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
